8ae4a00257ca2b800e618c537592ed186fc18aa0
laoniu-meow <vincent.niew@iCloud.com>
2025-05-06 22:19:15 +0800

    Commit Assignment 2.3

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "public" {
-  ami                         = "" # find the AMI ID of Amazon Linux 2023  instance_type               = "t2.micro"
+  ami                         = "ami-0e8ebb0ab254bb563" # find the AMI ID of Amazon Linux 2023  instance_type               = "t2.micro"
   instance_type               = "t2.micro"
-  subnet_id                   = ""  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+  subnet_id                   = "subnet-0a61b179394ae66c8"  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
   associate_public_ip_address = true
-  key_name                    = "" #Change to your keyname, e.g. jazeel-key-pair
+  key_name                    = "laoniu-pem" #Change to your keyname, e.g. jazeel-key-pair
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
  
   tags = {
-    Name = "ce10-laoniu-server"    #Prefix your own name, e.g. jazeel-ec2
+    Name = "${var.name}-ec2"    #Prefix your own name, e.g. jazeel-ec2
   }
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "ce10-laoniu-terraform-security-group" #Security group name, e.g. jazeel-terraform-security-group
   description = "Allow SSH inbound"
-  vpc_id      = ""  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+  vpc_id      = data.aws_vpc.selected.id  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+   type = string
+   description = "ce10-laoniu-server"
+}
